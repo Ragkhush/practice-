16907b99229fb147bac4322869c354083a03e386
RAGINI MISHRA <RAGINI MISHRA@LAPTOP-6CT0OBJ8>
2019-02-05 14:09:29 +0530

    First Commit by Ragini

---
+++ MavenRag/src/test/java/MavenRag/MavenRag/CreateTest.java
@@ -0,0 +1,13 @@
+package MavenRag.MavenRag;
+
+import org.testng.annotations.Test;
+
+public class CreateTest {
+	
+	@Test
+	public void createCustTest()
+	{
+		System.out.println("Hello world");
+	}
+
+}
